f2c32337ebee0600fbec383cef6cc1d02b256a8b
28GEngle <28GEngle@live.ytech.edu>
2026-09-01 14:54:29 -0400

    End of day commit

--- docpac_030309/Button Masher Game/EngleGrant/button-masher-game/game.js
+++ docpac_030309/Button Masher Game/EngleGrant/button-masher-game/game.js
@@ -1,5 +1,9 @@
+// LOCAL SETTINGS
+initialTime = 20
+//
+
 directions = {
-    "up": 0,
+    "down": 0,
     "down": 1,
     "left": 2,
     "right": 3
@@ -8,23 +12,64 @@ directions = {
 controllerStatusText = document.getElementById("controller");
 scoreText = document.getElementById("score");
 timeText = document.getElementById("time");
+startButton = document.getElementById("start");
+
+gameTime = initialTime
+direction = 0
+
+controller = false
 
 window.addEventListener("gamepadconnected",(listener) => {
+    controllerStatusText.textContent = "Detected";
+    requestAnimationFrame(pollGamepad)
+})
+
+window.addEventListener("gamepaddisconnected",(listener) => {
     controllerStatusText.textContent = "Detected"
-    console.log("Controller found")
-});
+    console.log("Controller lost")
+})
 
-gameTime = 5
+function pollGamepad() {
+    gp = navigator.getGamepads()[0]
 
-gametimeID = setInterval(() => {
+
+    gp.buttons.forEach((button, index) => {
+        if (button.pressed) {
+            buttonIndex = gp.buttons[index]
+            console.log(`Button ${index} was pressed`)
+        }
+    });
+
+    requestAnimationFrame(pollGamepad)
+}
+
+function endGame() {
+    console.log("end")
+    clearInterval(timeInterval)
+    clearInterval(directionInterval)
+    startButton.disabled = false
+}
+
+function startGame() {
+    startButton.disabled = true
+    timeText.textContent = gameTime
+    gameTime = initialTime
+    timeInterval = setInterval(timerTick, 1000)
+    directionInterval = setInterval(randomizeDirection, 2000)
+}
+
+
+
+function randomizeDirection() {
+    direction = Math.floor(Math.random()*3.99);
+    console.log(direction)
+}
+
+function timerTick() {
+    requestAnimationFrame(pollGamepad)
+    timeText.textContent = gameTime
+    gameTime -= 1
     if (gameTime <= 0) {
-        clearInterval()
-    } else {
-        gameTime -= 1
-        timeText.textContent = String(gameTime);
+        endGame()
     }
-},1000);
-
-directionID = setInterval(() => {
-    direction = Math.floor(Math.random*3.9)
-},2000);
+}
