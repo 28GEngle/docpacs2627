7848a65f219e13fb746dc049b212beeae10fc2fe
28GEngle <28GEngle@live.ytech.edu>
2026-09-03 09:16:51 -0400

    Removing other peoples files that are for some reason on my machine

--- docpac_012108/Unit1Lesson1/JenkinsEmmett/app.js
+++
@@ -1,5 +0,0 @@
-const hello = ["World", "Mr. Smith", "Node.JS"];
-for (let run = 1; run <= hello.length; run++) { console.log("Hello " + hello[run-1]); }
-console.log(1);
-console.log("3^2 * 7 - 4 =");
-console.log(3^2*7-4);
