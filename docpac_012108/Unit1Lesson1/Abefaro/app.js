33953679f02d3c43714e6e30046006cd43ff8641
28GEngle <28GEngle@live.ytech.edu>
2026-09-03 09:17:16 -0400

    Removing other peoples files that are for some reason on my machine

--- docpac_012108/Unit1Lesson1/Abefaro/app.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello World!")
-console.log("Abe is very cool")
-console.log(42)
-let stupid = 'True'
-console.log(stupid)
-console.log(10 + 5)
--- docpac_012108/Unit1Lesson1/PotterLogan/index.js
+++
@@ -1,7 +0,0 @@
-testvar = 456
-
-console.log("test1")
-console.log(3)
-console.log(testvar)
-console.log(1 / 3 + 6)
-console.log("Hello World!")
