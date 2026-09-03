24e06f4139f87d91a9fbd1ac80584e261cf8eda0
28GEngle <28GEngle@live.ytech.edu>
2026-09-02 22:02:52 -0400

    End of shift commit
    - Made the game functional
    - Added frame skipping for lower performing computers
    - Slightly modularized the game for future features

--- docpac_030309/Button Masher Game/EngleGrant/button-masher-game/game.js
+++ docpac_030309/Button Masher Game/EngleGrant/button-masher-game/game.js
@@ -1,24 +1,8 @@
-// LOCAL SETTINGS
-initialTime = 20
-//
-
-directions = {
-    "down": 0,
-    "down": 1,
-    "left": 2,
-    "right": 3
-}
-
-controllerStatusText = document.getElementById("controller");
-scoreText = document.getElementById("score");
-timeText = document.getElementById("time");
-startButton = document.getElementById("start");
-
-gameTime = initialTime
-direction = 0
-
-controller = false
-
+// Settings
+var initialTime = 25 // Duration of each game.
+var framerate = 2 // Increase if you are having performance issues.
+var msPerDirectionChange = 3000
+// --------
 window.addEventListener("gamepadconnected", (listener) => {
     controllerStatusText.textContent = "Detected";
     requestAnimationFrame(pollGamepad)
@@ -28,57 +12,170 @@ window.addEventListener("gamepaddisconnected", (listener) => {
     controllerStatusText.textContent = "Detected"
     console.log("Controller lost")
 })
+// --------
+var score = 0
+var scoringDirection = 0
+var gameTime = initialTime
 
-lastPress = NaN
-lastAxes = [NaN, NaN]
+var directions = {
+    "up": 0,
+    "down": 1,
+    "left": 2,
+    "right": 3
+}
 
+var controllerStatusText = document.getElementById("controller");
+var scoreText = document.getElementById("score");
+var timeText = document.getElementById("time");
+var directionText = document.getElementById("directionText")
+var startButton = document.getElementById("start");
+
+var controller = false
+var gameRunning = false
+
+var lastPress = NaN
+var lastAxes = [NaN, NaN]
+
+var buttonActivations = {
+    0: attemptAward,
+    9: startGame
+}
+
+var buttonStates = {
+    0: false,
+    9: false
+}
+
+frame = 0
 function pollGamepad() {
-    gp = navigator.getGamepads()[0]
-    gp.buttons.forEach((button, index) => {
-        if (button.pressed && (lastPress != index)) {
-            lastPress = Number(index);
-            console.log(`Button ${index} was pressed, lastPress = ${lastPress}`);
-        }
-    });
-    currentAxes = [gp.axes[0], gp.axes[1]]
+    frame++
 
-    if ((currentAxes[0] == lastAxes[0]) && (currentAxes[1] == lastAxes[1])) {
-        
-    } else {
-        console.log(`unique: ${currentAxes}, ${lastAxes}`)
-        lastAxes = currentAxes 
+    if (frame >= framerate) {
+        frame = 0
+        gp = navigator.getGamepads()[0]
+        gp.buttons.forEach((button, index) => {
+            currentAxes = [gp.axes[0], gp.axes[1]]
+
+            if (button.pressed && !buttonStates[index]) {
+                console.log(`${index} pressed`)
+
+                // If the button has a function that can be ran with arguments, run it with the arguments.
+                if (buttonActivations[0] && index == 0) {
+                    buttonActivations[0](currentAxes);
+                } // If the button has a function that can be ran without arguments, run it.
+                else if (buttonActivations[index]) {
+                    buttonActivations[index]();
+                }
+
+                buttonStates[index] = true
+            } else if (!button.pressed && buttonStates[index]) {
+                console.log(`${index} released`)
+                buttonStates[index] = false
+            }
+
+            if (
+                deadzone(currentAxes[0]) == deadzone(lastAxes[0]) &&
+                deadzone(currentAxes[1]) == deadzone(lastAxes[1])
+            ) {
+
+            } else {
+                console.log(`unique: ${currentAxes}`)
+                lastAxes = currentAxes
+            }
+        });
     }
-
     requestAnimationFrame(pollGamepad)
 }
 
 function endGame() {
-    console.log("end")
-    clearInterval(timeInterval)
-    clearInterval(directionInterval)
-    startButton.disabled = false
+    console.log("ending");
+    clearInterval(timeInterval);
+    clearInterval(directionInterval);
+
+    startButton.disabled = false;
+
+    gameRunning = false;
+}
+
+function deadzone(input, min = -0.3, max = 0.3) {
+    if (input < max && input > min) {
+        return 0
+    } else {
+        return input
+    }
 }
 
 function startGame() {
-    startButton.disabled = true
-    timeText.textContent = gameTime
-    gameTime = initialTime
-    timeInterval = setInterval(timerTick, 1000)
-    directionInterval = setInterval(randomizeDirection, 2000)
+    if (!gameRunning) {
+        gameTime = initialTime
+        score = 0
+
+        randomizeDirection()
+        timeInterval = setInterval(timerTick, 1000)
+        directionInterval = setInterval(randomizeDirection, msPerDirectionChange)
+
+        console.log("starting")
+        gameRunning = true;
+
+        startButton.disabled = true
+        timeText.textContent = initialTime
+    }
 }
 
+function gamepadDirection(axes) {
+    var currentDirection;
+    if (deadzone(axes[1]) > 0) {
+        currentDirection = directions.up
+    } else if (deadzone(axes[1]) < 0) {
+        currentDirection = directions.down
+    }
+    
+    if (deadzone(axes[0]) > 0) {
+        currentDirection = directions.left
+    } else if (deadzone(axes[0]) < 0) {
+        currentDirection = directions.right
+    }
 
+    return currentDirection
+}
 
+function attemptAward(axes) {
+    if (scoringDirection == gamepadDirection(axes) && gameRunning) {
+        score++
+        scoreText.textContent = score
+    }
+}
+
+function numberToDirectionString(input) {
+    if (input == 0) {
+        return "DOWN"
+    } else if (input == 1) {
+        return "UP"
+    } else if (input == 2) {
+        return "RIGHT"
+    } else if (input == 3) {
+        return "LEFT"
+    }
+}
+
+previousDirection = -1
 function randomizeDirection() {
-    direction = Math.floor(Math.random() * 3.99);
-    console.log(direction)
+    scoringDirection = Math.floor(Math.random() * 3.99);
+    if (scoringDirection == previousDirection) {
+        if ((scoringDirection + 1) >= 4) {
+            scoringDirection -= 1
+        } else {
+            scoringDirection += 1
+        }
+    }
+    previousDirection = scoringDirection
+    directionText.textContent = numberToDirectionString(scoringDirection)
 }
 
 function timerTick() {
-    requestAnimationFrame(pollGamepad)
-    timeText.textContent = gameTime
     gameTime -= 1
     if (gameTime <= 0) {
         endGame()
     }
+    timeText.textContent = gameTime
 }
